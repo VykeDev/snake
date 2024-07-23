1e97b777ed8042547128d104fa4f2dd1303bd5f4
Rift <hegide.dev@gmail.com>
2024-07-23 18:57:26 -0400

    snake

--- script.js
+++ script.js
@@ -4,9 +4,26 @@ canvas.height = window.innerHeight;
 
 let ctx = canvas.getContext("2d");
 
+function Snake() {
+  this.color = "blue";
+  this.size = 50;
+  this.length = 1;
+  this.x = 200;
+  this.y = 300;
+
+  this.draw = () => {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+    ctx.stroke();
+  };
+}
+let player = new Snake();
+
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  console.log(1);
+
+  player.draw();
 }
 animate();
