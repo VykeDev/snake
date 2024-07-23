eb292fee8cf25e3836ea24234017872e8e22dddf
Rift <hegide.dev@gmail.com>
2024-07-23 18:46:55 -0400

    game loop

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let ctx = canvas.getContext("2d");
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  console.log(1);
+}
+animate();
