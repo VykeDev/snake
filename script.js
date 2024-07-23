9fb1032604641e901a713bb54babda5129c8b06e
Rift <hegide.dev@gmail.com>
2024-07-23 19:03:41 -0400

    movement

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ canvas.height = window.innerHeight;
 
 let ctx = canvas.getContext("2d");
 
+document.addEventListener("keydown", function (event) {
+  // Handle the event here
+  if (event.key == "d" || event.key == "ArrowRight") {
+    console.log("right");
+    player.x += 5;
+  }
+  if (event.key == "a" || event.key == "ArrowLeft") {
+    console.log("left");
+    player.x -= 5;
+  }
+});
+
 function Snake() {
   this.color = "blue";
   this.size = 50;
   this.length = 1;
-  this.x = 200;
-  this.y = 300;
+  this.x = 20;
+  this.y = 30;
 
   this.draw = () => {
     ctx.beginPath();
